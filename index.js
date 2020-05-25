50c373c87d1de775aaf02d94e736cdb8e06bb4c2
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-05-25 18:33:31 +0300

    Node-HW-01

---
+++ index.js
@@ -0,0 +1,33 @@
+const argv = require("yargs").argv;
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+      
+    case "list":
+      listContacts().then(console.table);
+      break;
+
+    case "get":
+      getContactById(id).then(console.log);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
